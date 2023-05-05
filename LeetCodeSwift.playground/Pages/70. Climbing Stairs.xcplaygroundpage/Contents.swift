9dd01067d1423d36e0f2805c2605d493109ecf15
Danila Goriachkin <dndv279@gmail.com>
2023-05-05 10:12:36 +0300

    70. Climbing Stairs

--- LeetCodeSwift.playground/Pages/70. Climbing Stairs.xcplaygroundpage/Contents.swift
+++ LeetCodeSwift.playground/Pages/70. Climbing Stairs.xcplaygroundpage/Contents.swift
@@ -1,11 +1,22 @@
-class Solution {
+final class Solution {
     func climbStairs(_ n: Int) -> Int {
-        guard n != 0 else { return 0 }
+        guard n >= 0 else { return 0 }
         guard n != 1 else { return 1 }
         guard n != 2 else { return 2 }
         
-        return climbStairs(n - 2) + climbStairs(n - 1)
+        var a = 1
+        var b = 2
+        var result = 0
+        for _ in 3...n {
+            result = a + b
+            a = b
+            b = result
+        }
+
+        return result
     }
 }
 
 Solution().climbStairs(2)
+Solution().climbStairs(3)
+Solution().climbStairs(4)
