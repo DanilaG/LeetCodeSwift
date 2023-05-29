6cb645227279b70c8d7ebd36107c1951b42a018a
Danila Goriachkin <dndv279@gmail.com>
2023-05-29 19:39:07 +0600

    226. Invert Binary Tree

---
+++ LeetCodeSwift.playground/Pages/226. Invert Binary Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+final class Solution {
+  func invertTree(_ root: TreeNode?) -> TreeNode? {
+      guard let node = root else { return nil }
+      let buf = node.left
+      node.left = invertTree(node.right)
+      node.right = invertTree(buf)
+      return node
+  }
+}
+
+let tree = TreeNode(4, nil, nil)
+Solution().invertTree(tree)
