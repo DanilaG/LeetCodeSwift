cac473ab8f9dcdf3ebe7df5152d7a19cb7448a6c
Danila Goriachkin <dndv279@gmail.com>
2023-07-21 17:39:05 +0300

    2769. Find the Maximum Achievable Number

---
+++ LeetCodeSwift.playground/Pages/2769. Find the Maximum Achievable Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+final class Solution {
+    func theMaximumAchievableX(_ num: Int, _ t: Int) -> Int {
+        num + t * 2
+    }
+}
+
+Solution().theMaximumAchievableX(4, 1)
+Solution().theMaximumAchievableX(3, 2)
