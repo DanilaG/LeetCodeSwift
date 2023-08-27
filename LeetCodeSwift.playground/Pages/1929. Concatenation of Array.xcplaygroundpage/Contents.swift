6deacc50d7556fb6d2fe7076db70eac7bdffe543
Danila Goriachkin <dndv279@gmail.com>
2023-08-27 22:41:45 +1000

    1929. Concatenation of Array

---
+++ LeetCodeSwift.playground/Pages/1929. Concatenation of Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+final class Solution {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        nums + nums
+    }
+}
+
+Solution().getConcatenation([1,2,1])
+Solution().getConcatenation([1,3,2,1])
+
